4286e1e0a888c32da63a3129e875e6dfe731e12b
virgilseed <virgilseed999>
2015-11-03 13:17:22 +0800

    项目提交（后期还需优化：seed）

--- app/src/main/java/com/seed/caculator/MainActivity.java
+++ app/src/main/java/com/seed/caculator/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private CaculatoreDialog mCaculatoreDialog;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
